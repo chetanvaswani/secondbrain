76b4caa4786070f13731776db0382fa7da9a18fd
chetanvaswani <chetan.vaswani.cv@gmail.com>
2025-01-14 20:39:00 +0530

    password hash added

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import { PrismaClient } from '@prisma/client';
 import { userSchema, userInterface } from "../types/schema";
-import jwt from 'jsonwebtoken';
 import * as dotenv from 'dotenv';
+import bcrypt from 'bcrypt';
 dotenv.config();
 
 export const UserRouter = express.Router()
 const prisma = new PrismaClient();
+const secret: string = process.env.SECRET!;
 
 UserRouter.post('/signup', async (req, res) => {
     const response = userSchema.safeParse(req.body)
@@ -26,9 +27,21 @@ UserRouter.post('/signup', async (req, res) => {
         return
     }
 
-    const createUser = await prisma.users.create({ data: user })
-    res.send(createUser)
-    const token = jwt.sign({
-        createUser
-    }, 'secret')
+    const hash = await bcrypt.hash(user.password, 1)
+
+    const createUser = await prisma.users.create({ data: {
+        username: user.username,
+        password: hash
+    } })
+    // const token = jwt.sign({
+    //     id: createUser.id,
+    //     username: createUser.username,
+    //     createdAT: Date.now()
+    // }, secret)
+    // console.log(token)
+    // console.log(jwt.decode(token))
+    res.status(200).json({
+        success: true,
+        msg: "user created successfully!"
+    })
 })
